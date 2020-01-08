ee3bd718f4334620845260e634f74e6dde00e06b
Lorenz <lorwel@mailbox.org>
2020-01-08 16:38:53 +0100

    Update hetzner-failover.py

--- hetzner-failover.py
+++ hetzner-failover.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3 -u
 #
-# Copyright 2018 Lorenz Wellmer <lorwel@mailbox.org>
-#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
